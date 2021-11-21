9cf096f6414ef6af747aff22a32240f219176eb4
Eeshani Patel <eeshani.p01@gmail.com>
2021-11-21 15:42:46 +0530

    all other updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ContactComponent } from './contact/contact.component';
 import { DiyComponent } from './diy/diy.component';
 import { GuideComponent } from './guide/guide.component';
 import { HomeComponent } from './home/home.component';
+import { MerchandiseComponent } from './merchandise/merchandise.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HomeComponent } from './home/home.component';
     ContactComponent,
     DiyComponent,
     GuideComponent,
-    HomeComponent
+    HomeComponent,
+    MerchandiseComponent
   ],
   imports: [
     BrowserModule,
